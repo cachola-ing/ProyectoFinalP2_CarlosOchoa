3130826c4c6631091b72c18dc4e88c6fbb6cd694
Carlos <carlosochoa160808@gmail.com>
2026-09-21 22:27:20 -0600

    Usuario y Configuracion

---
+++ src/proyectofinalp2_carlosochoa/Configuracion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinalp2_carlosochoa;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Carlos Antonio
+ */
+public class Configuracion {
+    
+    Color colorFondo;
+    Color colorNavbar;
+    String fuente;
+    int tamanoFuente;
+    String imagenFondo;
+
+    public Configuracion(Color colorFondo, Color colorNavbar, String fuente, int tamanoFuente, String imagenFondo) {
+        this.colorFondo = colorFondo;
+        this.colorNavbar = colorNavbar;
+        this.fuente = fuente;
+        this.tamanoFuente = tamanoFuente;
+        this.imagenFondo = imagenFondo;
+    }
+    
+         
+}
---
+++ src/proyectofinalp2_carlosochoa/Usuario.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinalp2_carlosochoa;
+
+/**
+ *
+ * @author Carlos Antonio
+ */
+public class Usuario {
+    String nombre;
+    String contrasena;
+    String tipo;
+
+    public Usuario(String nombre, String contrasena, String tipo) {
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+}
